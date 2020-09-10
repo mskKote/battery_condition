5fe66a251b8496b0f9b550ba92a8be80ea9c8fc6
Попов Виталий <msk.vitaly@gmail.com>
2020-09-10 06:27:51 +0300

    generic value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { Timestamp } from 'rxjs';
 import { ServerService, totals } from './server.service';
 import { Component, OnInit } from '@angular/core';
 import { NgxChartsModule, ColorHelper } from '@swimlane/ngx-charts';
 import * as shape from 'd3-shape';
+import { ClassMethod } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,7 @@ import * as shape from 'd3-shape';
 })
 export class AppComponent implements OnInit {
   // Главный график
-  // yAxisTicksArr: any[] = [
-  //   0   , 0.05, 0.10, 
-  //   0.15, 0.20, 0.25, 
-  //   0.30, 0.35, 0.40, 
-  //   0.45, 0.50, 0.55,
-  //   0.60, 0.65, 0.70,
-  //   0.75, 0.80, 0.85,
-  //   0.90, 0.95, 1.00,
-  //   1.05 ];
-  //this.getArrY(1.75, 2.8, 0.05);
+   yAxisTicksArr: any[] = this.getArrY(0, 1.05, 0.15);
   showXAxis: boolean = true;
   showYAxis: boolean = true;
   gradient: boolean = true;
@@ -27,7 +20,7 @@ export class AppComponent implements OnInit {
   showXAxisLabel: boolean = true;
   showYAxisLabel: boolean = true;
   xAxisLabel: string = 'Пары батарей';
-  yAxisLabel: string = '% Заряда';
+  yAxisLabel: string = '<';
   animations: boolean = false;
   showDataLabel: boolean = true;
   showGridLines: boolean = false;
@@ -37,7 +30,6 @@ export class AppComponent implements OnInit {
   yAxisTickFormattingMulti(val: any) {
     return val + 1.75
   }
-  RightTickValues: string[] = ['0', '1']
 
   //Линиии
   yAxisTickFormattingLine(val: any) {
@@ -45,19 +37,31 @@ export class AppComponent implements OnInit {
     if(val == '1') { return 'Вкл'}
   }
   linearCurveCardinal = shape.curveCardinal;
-  linearCurveStep= shape.curveStep;
+  linearCurveStep= shape.curveStepAfter;
   amperTicks: any[] =  [0, 0.01, 0.02, 0.03, 0.04];
   xAxis: boolean = true;
   yAxis: boolean = true;
   timeline: boolean = true;
+  colorChange = {
+    domain: []
+  };
+  colorChange_Total = {
+    domain: []
+  };
+  colorChange_Temperature = {
+    domain: []
+  };
+  colorChange_ACDC = {
+    domain: []
+  };
   colorScheme = {
     domain: ['#ff0000', '#ffaf00', '#f9ff00', '#b0ff00', '#00ff00'],
   };
   schemeType: string = 'linear';
+  timeframe: string = 'Время, мин'; // Нужно изменять мс/cек/мин/час/день/неделя/месяц
 
   onSelect(data: any): void {
     //console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-    //this.genData();
   }
 
   onActivate(data: any): void {
@@ -84,13 +88,8 @@ export class AppComponent implements OnInit {
     "name": "Контактор",
     "series": []
   }];
-  multi:any[];
-  single: any[] = [
-    // {
-    //   name: 'Заряд батареи',
-    //   value: 80,
-    // },
-  ];
+  multi:any[] = [];
+  single: any[] = [];
   batteries: any[] = new Array(30);
   
   currentBattery: number = 0;
@@ -114,50 +113,84 @@ export class AppComponent implements OnInit {
     this.currentBattery = target;
   }
 
+  //---------------------------------------------------Раздел генерации значениц
   static randomDate(start:Date, end:Date): Date {
     return new Date(start.getTime() 
       + Math.random() * (end.getTime() - start.getTime()));
   }
-  genData() {    // Генерируем данные
-    // try{
-    //   console.log(ServerService.end.getTime());
-    //   console.log(ServerService.start.getTime());
-    //   const bn = this.server.getDataQuery(ServerService.start.getTime().toString(),ServerService.end.getTime().toString());
-    //   bn.then(b=>console.log(b));
-    // }
-    // catch{}
-    // this.multi = [];
-    // for (let i = 0; i < 15; i++) {
-    //   this.multi.push({
-    //     name: i + 1,
-    //     series: [
-    //       {
-    //         "name": "",
-    //         "value": (Math.random()*100).toFixed(2)
-    //       },
-    //       {
-    //         "name": ".",
-    //         "value": (Math.random()*100).toFixed(2)
-    //       }
-    //     ]
-    //   });
-    // }
-    // this.time_temp0[0].series.push({
-    //   "value": (Math.random()*1000).toFixed(2),
-    //   "name": AppComponent.randomDate(new Date(2012, 0, 1), new Date())
-    // });
-    // this.time_temp1[0].series.push({
-    //   "value": (Math.random()*1000).toFixed(2),
-    //   "name": AppComponent.randomDate(new Date(2012, 0, 1), new Date())
-    // });
+  
+  static randomNumber(start: number, end:number): number {
+    return start + Math.random()*(end - start)
+  }
+  // Изменяю значения, расчётом на прежнее
+  // volatile -- вероятность смены значения
+  static genBoolByPrevious(volatile): boolean {
+    return Math.random() > volatile; 
+  }
+  
+  // Изменяем значения, НО с сохранением тренда с вероятностью volatile
+  // Может изменяться на variability
+  // Разворачивается при min и max
+  // @ Изменяем volatile и variability, чтобы сделать график более спокойным/буйным
+  static getNumByPrevious(prev: number, volatile: number, variability: number, min: number, max: number): number {
+    // Предыдущее += знак*(изменяемость) 
+    prev += (Math.random() > volatile ? 1 : -1)*(Math.random()*variability);
+    // Мы достигли дна. Принудительно останавливаем падение/рост
+    if      (prev < min) { prev = min; }
+    else if (prev > max) { prev = max; }
+    // Приступаем к развороту тренда, когда будет 10% до минимума или максимума 
+    if      (prev < min + (max - min) * 0.1) { prev += (max - min) * AppComponent.randomNumber(0.03, 0.07); } // Прибавляем от 3% до 7%
+    else if (prev > max - (max - min) * 0.1) { prev -= (max - min) * AppComponent.randomNumber(0.03, 0.07); }
 
-    // let buff0 = this.time_temp0[0];
-    // this.time_temp0 = [buff0];
+    return prev;
+  }
+  //---------------------------------------------------
+  // Генерит 1 партию данных с указанным timestamp
+  contactor_gen: boolean;
+  balance_gen: boolean;
+  temperature_gen: number = AppComponent.randomNumber(-40, 60);
+  ACDC_gen: number =        AppComponent.randomNumber(0, 1000);
 
-    // let buff1 = this.time_temp1[0];
-    // this.time_temp1 = [buff1];
-    this.addTimePointTime0();
-    this.addTimePointTime1();
+  genData(timestamp: number) { // Генерит и рисует данные
+    // Значения на контактор и балансировку
+    this.contactor_gen = AppComponent.genBoolByPrevious(0.4);//Значение изменится с 40% вероятностью
+    this.balance_gen =   AppComponent.genBoolByPrevious(0.4);
+    this.temperature_gen = AppComponent.getNumByPrevious(this.temperature_gen, 0.4, 5, -40, 60)
+    this.ACDC_gen =        AppComponent.getNumByPrevious(this.ACDC_gen, 0.6, 100, 0, 2000);
+
+
+    this.addTimePointContractor({
+      "value": this.contactor_gen ? "1" : "0",
+      "name": new Date(timestamp)
+    });
+    this.addTimePointBalance({
+      "value": this.balance_gen ? "1" : "0",
+      "name": new Date(timestamp)
+    });
+    // Значения на температуру
+    this.addTimePointTime0({
+      "value": `${this.temperature_gen}`,
+      "name": new Date(timestamp)
+    });
+    //this.colorChange_Temperature.domain = [this.temperature_gen > 0 ? 'red' : 'blue']
+    // Значения на силу тока
+    this.addTimePointTime1({
+      "value": `${this.ACDC_gen}`,
+      "name": new Date(timestamp)
+    });
+
+    // Меняем цвета
+    this.colorChange.domain = [
+      ['#ff0000']
+    ];
+    this.colorChange_Temperature.domain = [
+      ['#18D8FF', '#AFDAFC', '#1F75FE','#0000FF', '#CC0605', '#C10020']
+      [Math.round(Math.abs((this.temperature_gen + 20) / 20))]
+    ];
+    this.colorChange_ACDC.domain = [
+      ['#000000',  '#011465', '#1F75FE','#1845FF', '#1888FF', '#18D8FF']
+      [Math.round((this.ACDC_gen / 2000 * 100 + 1) / 20)]
+    ];
   }
 
   addTimePointTime0(point = {
@@ -190,13 +223,52 @@ export class AppComponent implements OnInit {
 
   
   constructor(public server: ServerService){
-    //this.genData();
-    //setInterval( () => {this.genData();}, 4000);
+    // Генерируем данные для 30 батарей --> общее
+    // Графикс c 30 батареями и total_voltage
+    let total_voltage = 0;
+    for (let j = 0; j < 30; j+=2) {
+      const battery1 = AppComponent.randomNumber(1.75, 2.8);
+      const battery2 = AppComponent.randomNumber(1.75, 2.8);
+      this.multi.push({
+        "name": j / 2 + 1,
+        "series": [
+          {
+            "name": "Battery №" + j,
+            "value": battery1 - 1.75
+          }, {
+            "name": "Battery №" + j + 1,
+            "value": battery2 - 1.75
+          }
+      ]});
+      total_voltage += battery1 + battery2;
+    }
+    let total_voltage_percent = 
+    this.single.push({
+      name: 'Заряд батареи',
+      value: Math.floor((total_voltage - 30*1.75) / (30 * 1.05) * 100)
+    });
+
+    this.colorChange_Total.domain = [
+      ['#ff0000', '#ffaf00', '#f9ff00', '#b0ff00', '#00ff00']
+      [Math.round(((total_voltage - 30*1.75) / (30 * 1.05) * 100 + 1)/20)]
+    ];
+
+    // Данные для остальных графиков
+    for (let i = 10; i > 0; i--) {
+      this.genData(+Date.now() - 1000 * 60 * i);
+    }
+      
+
+    // setInterval( () => {
+    //   for (let i = 10; i > 0; i--) {
+    //     this.genData(+Date.now() - 1000 * 60 * i);
+    //   }
+    // }, 1000);
   }
 
   ngOnInit(): void {
-    this.request();
-    setInterval(() => { this.request(); } , 1000)
+    // this.request();
+    // setInterval(() => { this.request(); } , 1000)
   }
 
   request()  {  
