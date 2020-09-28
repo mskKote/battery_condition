be919f95c4a3b853af57c5780ad1c366d80c7592
Попов Виталий <msk.vitaly@gmail.com>
2020-09-28 03:05:36 +0300

    fix total_charge chart and added data to 3x10 battaries chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -152,34 +152,41 @@ export class AppComponent implements OnInit {
         series: [],
       }
     ];
-    this.multi_ACDC = [];
-    for (let i = 0; i < 10; i++) {
-      this.multi_ACDC.push({
-        name: 'Сила тока ' + (i + 1),
-        series: [],
-      });
+    this.multi_ACDC_1_10 = [];
+    this.multi_ACDC_11_20 = [];
+    this.multi_ACDC_21_30 = [];
+
+    for (let i = 0; i < 30; i++) {
+      if (i < 10) 
+        this.multi_ACDC_1_10.push({
+          name: 'Сила тока ' + (i + 1),
+          series: []});
+      else if (i < 20)
+        this.multi_ACDC_11_20.push({
+          name: 'Сила тока ' + (i + 1),
+          series: []});
+      else 
+        this.multi_ACDC_21_30.push({
+          name: 'Сила тока ' + (i + 1),
+          series: []});
     }
 
     this.ACDC = [{
         name: 'Сила тока',
-        series: [],
-      },
-    ];
+        series: []
+    }];
     this.single_ACDC = [{
       name: 'Заряд батареи',
-      series: [],
-    },
-  ];
+      series: []
+    }];
     this.balance = [{
         name: 'Балансировка',
-        series: [],
-      },
-    ];
+        series: []
+    }];
     this.contactor = [{
         name: 'Контактор',
-        series: [],
-      },
-    ];
+        series: []
+    }];
     this.multi = [];
     this.single = [];
   }
@@ -214,7 +221,7 @@ export class AppComponent implements OnInit {
     return (val + 1.75).toFixed(2) + 'V';
   }
   yAxisTickFormattingMultiACDC(val: any) {
-    return val + 'V';
+    return (+val).toFixed(3) + 'V';
   }
   yAxisTickFormattingMulti2(val: any) {
     return (52.5 + (84 - 52.5) * (val / 100)).toFixed(2) + 'V';
@@ -254,175 +261,178 @@ export class AppComponent implements OnInit {
   balance: any[];
   contactor: any[];
   multi: any[] = [];
-  multi_ACDC: any[] = [];
+  multi_ACDC_1_10: any[] = [];
+  multi_ACDC_11_20: any[] = [];
+  multi_ACDC_21_30: any[] = [];
   single: any[] = [];
   single_ACDC: any[] = [];
   batteries: any[] = new Array(30);
+  tooltipText = 'Баттарея №';
+
 
   //---------------------------------------------------Раздел генерации значений
-  static randomDate(start: Date, end: Date): Date {
-    return new Date(
-      start.getTime() + Math.random() * (end.getTime() - start.getTime())
-    );
-  }
-  static randomNumber(start: number, end: number): number {
-    return start + Math.random() * (end - start);
-  }
+  // static randomDate(start: Date, end: Date): Date {
+  //   return new Date(
+  //     start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  //   );
+  // }
+  // static randomNumber(start: number, end: number): number {
+  //   return start + Math.random() * (end - start);
+  // }
   // Изменяю значения, расчётом на прежнее
   // volatile -- вероятность смены значения
-  static genBoolByPrevious(volatile): boolean {
-    return Math.random() > volatile;
-  }
+  // static genBoolByPrevious(volatile): boolean {
+  //   return Math.random() > volatile;
+  // }
 
   // Изменяем значения, НО с сохранением тренда с вероятностью volatile
   // Может изменяться на variability
   // Разворачивается при min и max
   // @ Изменяем volatile и variability, чтобы сделать график более спокойным/буйным
-  static getNumByPrevious(
-    prev: number,
-    volatile: number,
-    variability: number,
-    min: number,
-    max: number
-  ): number {
-    // Предыдущее += знак*(изменяемость)
-    prev += (Math.random() > volatile ? 1 : -1) * (Math.random() * variability);
-    // Мы достигли дна. Принудительно останавливаем падение/рост
-    if (prev < min) {
-      prev = min;
-    } else if (prev > max) {
-      prev = max;
-    }
-    // Приступаем к развороту тренда, когда будет 10% до минимума или максимума
-    if (prev < min + (max - min) * 0.1) {
-      prev += (max - min) * AppComponent.randomNumber(0.03, 0.07);
-    } // Прибавляем от 3% до 7%
-    else if (prev > max - (max - min) * 0.1) {
-      prev -= (max - min) * AppComponent.randomNumber(0.03, 0.07);
-    }
+  // static getNumByPrevious(
+  //   prev: number,
+  //   volatile: number,
+  //   variability: number,
+  //   min: number,
+  //   max: number
+  // ): number {
+  //   // Предыдущее += знак*(изменяемость)
+  //   prev += (Math.random() > volatile ? 1 : -1) * (Math.random() * variability);
+  //   // Мы достигли дна. Принудительно останавливаем падение/рост
+  //   if (prev < min) {
+  //     prev = min;
+  //   } else if (prev > max) {
+  //     prev = max;
+  //   }
+  //   // Приступаем к развороту тренда, когда будет 10% до минимума или максимума
+  //   if (prev < min + (max - min) * 0.1) {
+  //     prev += (max - min) * AppComponent.randomNumber(0.03, 0.07);
+  //   } // Прибавляем от 3% до 7%
+  //   else if (prev > max - (max - min) * 0.1) {
+  //     prev -= (max - min) * AppComponent.randomNumber(0.03, 0.07);
+  //   }
 
-    return prev;
-  }
+  //   return prev;
+  // }
   // Генерит 1 партию данных с указанным timestamp
-  temperature_gen: number[] = [
-    // Можно вручную задать стартовые значения
-    AppComponent.randomNumber(-40, 60),
-    AppComponent.randomNumber(-40, 60),
-    AppComponent.randomNumber(-40, 60),
-    AppComponent.randomNumber(-40, 60),
-    AppComponent.randomNumber(-40, 60),
-  ];
-  ACDC_gen: number = AppComponent.randomNumber(0, 1000);
+  // temperature_gen: number[] = [
+  //   // Можно вручную задать стартовые значения
+  //   AppComponent.randomNumber(-40, 60),
+  //   AppComponent.randomNumber(-40, 60),
+  //   AppComponent.randomNumber(-40, 60),
+  //   AppComponent.randomNumber(-40, 60),
+  //   AppComponent.randomNumber(-40, 60),
+  // ];
+  // ACDC_gen: number = AppComponent.randomNumber(0, 1000);
   // Итерация значений нужна, чтобы создавать кастомные изменения на графике
-  iter: number = 0;
+  // iter: number = 0;
 
-  total_voltage: number = 52.5;
-  tooltipText = 'Баттарея №';
+  // total_voltage: number = 52.5;
 
-  genData(timestamp: number) {
+  // genData(timestamp: number) {
 
-    // Закидываем значения на график
-    this.addTimePoint(this.contactor, {
-      value: AppComponent.genBoolByPrevious(0.4) ? '1' : '0',
-      name: new Date(timestamp),
-    });
-    this.addTimePoint(this.balance, {
-      value: AppComponent.genBoolByPrevious(0.4) ? '1' : '0',
-      name: new Date(timestamp),
-    });
-    //---------Создаём параметры графиков
-    //3 примерно равных значения (+-1-3) + 1 сильный пик
-    if (this.iter < 5)
-      this.temperature_gen[0] = AppComponent.getNumByPrevious(this.temperature_gen[0], 0.7, 4, -40, 60);
-    else if (this.iter == 6 || this.iter == 8)
-      this.temperature_gen[0] += (Math.random() < 0.5 ? 1 : -1) * 40;
-    else
-      this.temperature_gen[0] = AppComponent.getNumByPrevious(this.temperature_gen[0], 0.5, 10, -40, 60);
+  //   // Закидываем значения на график
+  //   this.addTimePoint(this.contactor, {
+  //     value: AppComponent.genBoolByPrevious(0.4) ? '1' : '0',
+  //     name: new Date(timestamp),
+  //   });
+  //   this.addTimePoint(this.balance, {
+  //     value: AppComponent.genBoolByPrevious(0.4) ? '1' : '0',
+  //     name: new Date(timestamp),
+  //   });
+  //   //---------Создаём параметры графиков
+  //   //3 примерно равных значения (+-1-3) + 1 сильный пик
+  //   if (this.iter < 5)
+  //     this.temperature_gen[0] = AppComponent.getNumByPrevious(this.temperature_gen[0], 0.7, 4, -40, 60);
+  //   else if (this.iter == 6 || this.iter == 8)
+  //     this.temperature_gen[0] += (Math.random() < 0.5 ? 1 : -1) * 40;
+  //   else
+  //     this.temperature_gen[0] = AppComponent.getNumByPrevious(this.temperature_gen[0], 0.5, 10, -40, 60);
 
-    if (this.temperature_gen[0] < -40) this.temperature_gen[0] = -40;
-    else if (this.temperature_gen[0] > 60) this.temperature_gen[0] = 60;
-    // Все равные значения (+-1)
-    this.temperature_gen[1] = AppComponent.getNumByPrevious(this.temperature_gen[1], 0.75, 1.5, -40, 60);
-    // Только пики +-30
-    this.temperature_gen[2] = AppComponent.getNumByPrevious(this.temperature_gen[2], 0.8, 30, -40, 60);
-    // Равномерно растущий и убывающий графики
-    this.temperature_gen[3] = AppComponent.getNumByPrevious(this.temperature_gen[3], 0.3, 5, -40, 60);
-    this.temperature_gen[4] = AppComponent.getNumByPrevious(this.temperature_gen[4], 0.3, 5, -40, 60);
+  //   if (this.temperature_gen[0] < -40) this.temperature_gen[0] = -40;
+  //   else if (this.temperature_gen[0] > 60) this.temperature_gen[0] = 60;
+  //   // Все равные значения (+-1)
+  //   this.temperature_gen[1] = AppComponent.getNumByPrevious(this.temperature_gen[1], 0.75, 1.5, -40, 60);
+  //   // Только пики +-30
+  //   this.temperature_gen[2] = AppComponent.getNumByPrevious(this.temperature_gen[2], 0.8, 30, -40, 60);
+  //   // Равномерно растущий и убывающий графики
+  //   this.temperature_gen[3] = AppComponent.getNumByPrevious(this.temperature_gen[3], 0.3, 5, -40, 60);
+  //   this.temperature_gen[4] = AppComponent.getNumByPrevious(this.temperature_gen[4], 0.3, 5, -40, 60);
 
-    for (let i = 0; i < this.time_temp0.length; i++) {
-      this.time_temp0[i].series.push({
-        value: `${Math.round(this.temperature_gen[i] * 100) / 100}`,
-        name: new Date(timestamp),
-      });
-    }
+  //   for (let i = 0; i < this.time_temp0.length; i++) {
+  //     this.time_temp0[i].series.push({
+  //       value: `${Math.round(this.temperature_gen[i] * 100) / 100}`,
+  //       name: new Date(timestamp),
+  //     });
+  //   }
 
-    if (this.iter < 6)
-      this.ACDC_gen = AppComponent.getNumByPrevious(this.ACDC_gen, 0.7, 10, 15, 40);
-    else
-      this.ACDC_gen = AppComponent.getNumByPrevious(this.ACDC_gen, 0.7, 10, 15, 25);
-    this.addTimePoint(this.ACDC, {
-      value: `${this.ACDC_gen}`,
-      name: new Date(timestamp),
-    });
+  //   if (this.iter < 6)
+  //     this.ACDC_gen = AppComponent.getNumByPrevious(this.ACDC_gen, 0.7, 10, 15, 40);
+  //   else
+  //     this.ACDC_gen = AppComponent.getNumByPrevious(this.ACDC_gen, 0.7, 10, 15, 25);
+  //   this.addTimePoint(this.ACDC, {
+  //     value: `${this.ACDC_gen}`,
+  //     name: new Date(timestamp),
+  //   });
 
-    // Меняем цвета -- он вроде бы не обновляет значения...
-    this.colorChange.domain = ['#ff0000'];
+  //   // Меняем цвета -- он вроде бы не обновляет значения...
+  //   this.colorChange.domain = ['#ff0000'];
 
-    this.colorChange_ACDC.domain = [];
-    this.colorChange_ACDC.domain.push([
-      ['#000000', '#011465', '#1F75FE', '#1845FF', '#1888FF', '#18D8FF'][
-        Math.round((this.ACDC_gen - 15) / 5)
-      ],
-    ]);
-    this.iter++;
-  }
-  genGlobalCharts(
-    start = +Date.now() - 1000 * 60 * 10,
-    end = Date.now(),
-    amount = 10
-  ) {
-    // Генерируем данные для 30 батарей --> общее
-    // Графикс c 30 батареями и total_voltage
-    this.total_voltage = 0;
+  //   this.colorChange_ACDC.domain = [];
+  //   this.colorChange_ACDC.domain.push([
+  //     ['#000000', '#011465', '#1F75FE', '#1845FF', '#1888FF', '#18D8FF'][
+  //       Math.round((this.ACDC_gen - 15) / 5)
+  //     ],
+  //   ]);
+  //   this.iter++;
+  // }
+  // genGlobalCharts(
+  //   start = +Date.now() - 1000 * 60 * 10,
+  //   end = Date.now(),
+  //   amount = 10
+  // ) {
+  //   // Генерируем данные для 30 батарей --> общее
+  //   // Графикс c 30 батареями и total_voltage
+  //   this.total_voltage = 0;
 
-    for (let j = 0; j < 15; j++) {
-      const battery1 = AppComponent.randomNumber(1.75, 2.8);
-      const battery2 = AppComponent.randomNumber(1.75, 2.8);
+  //   for (let j = 0; j < 15; j++) {
+  //     const battery1 = AppComponent.randomNumber(1.75, 2.8);
+  //     const battery2 = AppComponent.randomNumber(1.75, 2.8);
 
-      this.multi.push({
-        name: (j + 1),
-        series: [
-          {
-            name: 'first',
-            value: battery1 - 1.75,
-            number: j * 2,
-          },
-          {
-            name: 'second',
-            value: battery2 - 1.75,
-            number: j * 2 + 1,
-          },
-        ],
-      });
+  //     this.multi.push({
+  //       name: (j + 1),
+  //       series: [
+  //         {
+  //           name: 'first',
+  //           value: battery1 - 1.75,
+  //           number: j * 2,
+  //         },
+  //         {
+  //           name: 'second',
+  //           value: battery2 - 1.75,
+  //           number: j * 2 + 1,
+  //         },
+  //       ],
+  //     });
 
-      this.total_voltage += battery1 + battery2;
-    }
-    this.single.push({
-      name: 'Заряд батареи',
-      value: Math.floor(((this.total_voltage - 30 * 1.75) / (30 * 1.05)) * 100),
-    });
-    // Цвет графика
-    this.colorChange_Total.domain = [
-      ['#ff0000', '#ffaf00', '#f9ff00', '#b0ff00', '#00ff00'][
-        Math.round(((this.total_voltage - 30 * 1.75) / (30 * 1.05)) * 5 - 1)
-      ],
-    ];
+  //     this.total_voltage += battery1 + battery2;
+  //   }
+  //   this.single.push({
+  //     name: 'Заряд батареи',
+  //     value: Math.floor(((this.total_voltage - 30 * 1.75) / (30 * 1.05)) * 100),
+  //   });
+  //   // Цвет графика
+  //   this.colorChange_Total.domain = [
+  //     ['#ff0000', '#ffaf00', '#f9ff00', '#b0ff00', '#00ff00'][
+  //       Math.round(((this.total_voltage - 30 * 1.75) / (30 * 1.05)) * 5 - 1)
+  //     ],
+  //   ];
 
-    // Данные для остальных графиков
-    for (let i = amount; i > 0; i--) {
-      this.genData(end - ((end - start) / amount) * i);
-    }
-  }
+  //   // Данные для остальных графиков
+  //   for (let i = amount; i > 0; i--) {
+  //     this.genData(end - ((end - start) / amount) * i);
+  //   }
+  // }
 
   addTimePoint(chart, point, index = 0) {
     chart[index].series.push(point);
@@ -537,7 +547,7 @@ export class AppComponent implements OnInit {
     // console.log("Дата из запроса: ", new Date(data.timestamp*1000));
       // console.groupCollapsed('data from JSON')
 
-      // console.log('dataArray >> ', dataArray);
+      //console.log('dataArray >> ', dataArray);
       // console.log('voltages :>> ', voltages);
       // console.log('newACDC >> ', newACDC);
 
@@ -582,9 +592,9 @@ export class AppComponent implements OnInit {
         name: 'Заряд батареи',
         value: Math.floor(((total_voltage_value - 30 * 1.75) / (30 * 1.05)) * 100),
       });
-      
+      console.log(total_voltage_value);
       this.addTimePoint(this.single_ACDC, {
-        value: `${newACDC}`,
+        value: `${total_voltage_value}`,
         name: new Date(timestamp*1000),
       });
       
@@ -626,14 +636,25 @@ export class AppComponent implements OnInit {
         });
       }
       //------------------Батареи
-      for (let i = 0; i < 10; i++) {
-        this.multi_ACDC[i].series.push({
-          value: `${Math.floor((voltages[i] - 1.75) * 100) / 100}`,
-          name: new Date(timestamp*1000),
-        });
+      for (let i = 0; i < 30; i++) {
+        if (i < 10) 
+          this.multi_ACDC_1_10[i].series.push({
+            value: `${Math.floor((voltages[i] - 1.75) * 100) / 100}`,
+            name: new Date(timestamp*1000)
+          });
+        else if (i < 20)
+          this.multi_ACDC_11_20[i % 10].series.push({
+            value: `${Math.floor((voltages[i] - 1.75) * 100) / 100}`,
+            name: new Date(timestamp*1000)
+          });
+        else 
+          this.multi_ACDC_21_30[i % 10].series.push({
+            value: `${Math.floor((voltages[i] - 1.75) * 100) / 100}`,
+            name: new Date(timestamp*1000)
+          });
       }
 
-      // Добавление значений
+      //==========================Добавление значений на графики
       let buff: any[];
       buff = this.ACDC[0].series;
       this.ACDC = [{
@@ -669,20 +690,48 @@ export class AppComponent implements OnInit {
       }];
       ////////////////////////////////
       buff = []; 
-      for (let i = 0; i < 10; i++) {
-        buff.push(this.multi_ACDC[i].series);
+      for (let i = 0; i < 30; i++) {
+        if (i < 10) 
+          buff.push(this.multi_ACDC_1_10[i].series);
+        else if (i < 20)
+          buff.push(this.multi_ACDC_11_20[i % 10].series);
+        else 
+          buff.push(this.multi_ACDC_21_30[i % 10].series);
       }
-      this.multi_ACDC = [];
-      for (let i = 0; i < 10; i++) {
-        this.multi_ACDC.push({
-          name: 'Батарея №' + (i + 1),
-          series: [...buff[i]],
-        });
+      this.multi_ACDC_1_10 = [];
+      this.multi_ACDC_11_20 = [];
+      this.multi_ACDC_21_30 = [];
+
+      for (let i = 0; i < 30; i++) {
+        if (i < 10) 
+          this.multi_ACDC_1_10.push({
+            name: 'Батарея №' + (i + 1),
+            series: [...buff[i]],
+          });
+        else if (i < 20)
+          this.multi_ACDC_11_20.push({
+            name: 'Батарея №' + (i + 1),
+            series: [...buff[i]],
+          });
+        else 
+          this.multi_ACDC_21_30.push({
+            name: 'Батарея №' + (i + 1),
+            series: [...buff[i]],
+          });
       }
 
+      // for (let i = 0; i < 10; i++) {
+      //   this.multi_ACDC_1_10.push({
+      //     name: 'Батарея №' + (i + 1),
+      //     series: [...buff[i]],
+      //   });
+      // }
+
+
+      ////////////////////////////////
       buff = this.single_ACDC[0].series;
       this.single_ACDC = [{
-        name: 'Сила тока',
+        name: 'Заряд батареи',
         series: [...buff],
       }];
       
